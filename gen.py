4cbad22d123abbf27a8f1e92f0aa9aac71afdb50
V$nny <108474407+VinnyRage@users.noreply.github.com>
2022-11-18 17:38:13 +0100

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,5 +1,6 @@
 import qrcode
 import image
+from colorama import Fore
 
 qr = qrcode.QRCode(
     version=1,
@@ -7,8 +8,17 @@ qr = qrcode.QRCode(
     box_size=10,
     border=4,
 )
-qr.add_data('LINK HERE')
+qr.add_data('https://www.youtube.com/channel/UCillU39POZVNHLLkKo2iD6w')
 qr.make(fit=True)
+
+print(Fore.RED + '''
+    ╔════════════════════════════════════════════╗
+    ║                                            ║ 
+    ║       QR CODE GENERATO CON SUCCESSO!       ║ 
+    ║                                            ║ 
+    ╚════════════════════════════════════════════╝
+				''',Fore.RED, Fore.WHITE, Fore.BLACK)
+
 
 img = qr.make_image()
 img.save("testqr.png")
